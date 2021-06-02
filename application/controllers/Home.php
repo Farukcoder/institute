36c771aea8dfa7dfeae5bac309ff84a3326c507f
Mkshoikot <mksoikotbhuiyan117@gmail.com>
2021-06-02 14:20:12 +0600

    one page modify done

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -6,12 +6,7 @@ class Home extends CI_Controller {
 	
 	public function index()
 	{
-		$this->load->view('header');
-		$this->load->view('leftmenu');
-		$this->load->view('downmenu');
 		$this->load->view('home');
-		$this->load->view('si');
-		$this->load->view('footer');
 	}
 
 }
--- application/views/header.php
+++ application/views/header.php
@@ -1,6 +1,65 @@
+<style>
+.menubar ul li:hover .submenu {
+    display: block;
+    position: absolute;
+    margin-top: 15px;
+    margin-left: -15px;
+}
+</style>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <base href="<?php echo base_url();?>" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="x-author" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7" />
+    <meta name="robots" content="noodp">
+    <title>GOPALGONJ POLYTECHNIC INSTITUTE</title>
+    <link rel="shortcut icon" href="assets_one/img/favicon.ico" type="image/x-icon" />
+    <link href="assets_one/css/bootstrap.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <link href="assets_one/css/flexslider.css" rel="stylesheet" />
+    <link href="assets_one/css/animate.css" rel="stylesheet" />
+    <link href="assets_one/css/slick.css" rel="stylesheet">
+    <link href="assets_one/css/slick-theme.css" rel="stylesheet">
+    <link href="assets_one/css/jquery.fs.boxer.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets_one/css/mobilenav.css">
+    <!-- Theme skin -->
+    <link href="assets_one/skins/default.css" rel="stylesheet" />
+    <!-- <link href="assets_one/css/style.css" rel="stylesheet" /> -->
+    <!-- <link rel="stylesheet" href="assets_one/css/bn.css"> -->
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <!--  <link rel="stylesheet" media="screen,projection" type="text/css" href="datepicker/jquery-ui.css" /> -->
+    <!--<script src="datepicker/jquery-1.9.1.js"></script>-->
+
+    <!-- Owl Carousel Assets -->
+    <link href="owl-carousel/owl.carousel.css" rel="stylesheet">
+    <link href="owl-carousel/owl.theme.css" rel="stylesheet">
+
+    <!--- picture uplaod js ---->
+    <script type="text/javascript" src="library/upload/ajaxupload.js"></script>
+
+    <!--- ajax request function for data serching----->
+    <script type="text/javascript" src="library/ajax_req.js"></script>
+
+    <!--  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>-->
+    <script src="js/google.js"></script>
+
+    <!-- <style>
+    body{color:font-family:solaimanlipi, "Times New Roman", Times, serif !important; color:black !important;}
+  </style> -->
+
+</head>
+
 <header style="padding-left:120px;">
-<div class="header" style="height:300px;width:90%;background:white;padding-left:20px">
-<h3><img src="assets/img/q.jpg" class="img-responsive" style="height:300px;width:100%;padding:15px" >
+<div class="header">
+<h3><img class="responsive_slide" src="assets/img/q.jpg">
 
 </div>
 </header>
